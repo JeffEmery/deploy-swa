0129809dd58b54735a19d729ddfffae346d3e7bb
Jeff Emery <jemery@envisionradiology.com>
2022-05-11 16:51:12 -0600

    remove def index.html add about route

---
+++ app/routes/about.tsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>About Remix</h1>
+      <ul>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            About Remix
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
